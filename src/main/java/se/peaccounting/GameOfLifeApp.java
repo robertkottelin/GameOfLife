b91d2cdd13d328cc15cd9eda739402aaf920bb97
robertkottelin <robert.kottelin@gmail.com>
2023-03-24 21:07:47 +0100

    final updates. Refactored code, commented, added test cases for game logic

--- src/main/java/se/peaccounting/GameOfLifeApp.java
+++ src/main/java/se/peaccounting/GameOfLifeApp.java
@@ -1,4 +1,5 @@
 package se.peaccounting;
+
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
@@ -9,33 +10,53 @@ import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.List;
+
+/**
+ * The main application class for Conway's Game of Life.
+ */
 public class GameOfLifeApp extends GameApplication {
 
-    protected int CELL_SIZE = 10;
-    protected int GRID_WIDTH = 80;
-    protected int GRID_HEIGHT = 60;
-    protected double UPDATE_STATE_INTERVAL = 0.1;
+    private static final int CELL_SIZE = 10;
+    private static final int GRID_WIDTH = 80;
+    private static final int GRID_HEIGHT = 60;
+    private static final double UPDATE_STATE_INTERVAL = 0.1; // How often (seconds) the generations update
 
-    protected List<Entity> cells = new ArrayList<>();
+    private final List<Entity> cells = new ArrayList<>();
 
+    /**
+     * Initializes the game settings.
+     *
+     * @param settings the game settings object
+     */
     @Override
     protected void initSettings(GameSettings settings) {
         configureSettings(settings);
     }
 
+    /**
+     * Initializes the game by creating and attaching cells and scheduling game state updates.
+     */
     @Override
     protected void initGame() {
         createAndAttachCells();
         scheduleGameStateUpdates();
     }
 
-    protected void configureSettings(GameSettings settings) {
+    /**
+     * Configures the game settings.
+     *
+     * @param settings the game settings object
+     */
+    private void configureSettings(GameSettings settings) {
         settings.setWidth(GRID_WIDTH * CELL_SIZE);
         settings.setHeight(GRID_HEIGHT * CELL_SIZE);
         settings.setTitle("Conway's Game of Life");
     }
 
-    protected void createAndAttachCells() {
+    /**
+     * Creates and attaches cells to the game.
+     */
+    private void createAndAttachCells() {
         for (int y = 0; y < GRID_HEIGHT; y++) {
             for (int x = 0; x < GRID_WIDTH; x++) {
                 Entity cell = createCell(x, y);
@@ -44,7 +65,14 @@ public class GameOfLifeApp extends GameApplication {
         }
     }
 
-    protected Entity createCell(int x, int y) {
+    /**
+     * Creates a cell entity at the specified position.
+     *
+     * @param x the x-coordinate of the cell
+     * @param y the y-coordinate of the cell
+     * @return the created cell entity
+     */
+    private Entity createCell(int x, int y) {
         return FXGL.entityBuilder()
                 .at(x * CELL_SIZE, y * CELL_SIZE)
                 .view(new Rectangle(CELL_SIZE, CELL_SIZE, Color.BLACK))
@@ -52,69 +80,22 @@ public class GameOfLifeApp extends GameApplication {
                 .buildAndAttach();
     }
 
-    protected void scheduleGameStateUpdates() {
+    /**
+     * Schedules the game state updates at the specified interval.
+     */
+    private void scheduleGameStateUpdates() {
         FXGL.run(() -> {
-            List<Boolean> nextGeneration = computeNextGeneration();
-
-            updateCells(nextGeneration);
+            // Call the computeNextGeneration method from the GameLogic class
+            List<Boolean> nextGeneration = GameLogic.computeNextGeneration(cells, CELL_SIZE, GRID_WIDTH, GRID_HEIGHT);
+            GameLogic.updateCells(cells, nextGeneration, CELL_SIZE);
         }, Duration.seconds(UPDATE_STATE_INTERVAL));
     }
 
-    protected List<Boolean> computeNextGeneration() {
-        List<Boolean> nextGeneration = new ArrayList<>();
-
-        for (Entity cell : cells) {
-            int x = (int) (cell.getX() / CELL_SIZE);
-            int y = (int) (cell.getY() / CELL_SIZE);
-
-            int aliveNeighbors = countAliveNeighbors(x, y);
-
-            boolean alive = cell.getBoolean("alive");
-            boolean nextAlive = (alive && (aliveNeighbors == 2 || aliveNeighbors == 3)) || (!alive && aliveNeighbors == 3);
-
-            nextGeneration.add(nextAlive);
-        }
-
-        return nextGeneration;
-    }
-
-    protected void updateCells(List<Boolean> nextGeneration) {
-        for (int i = 0; i < cells.size(); i++) {
-            Entity cell = cells.get(i);
-            boolean nextAlive = nextGeneration.get(i);
-
-            cell.setProperty("alive", nextAlive);
-            cell.getViewComponent().getChildren().forEach(node -> {
-                Rectangle rect = (Rectangle) node;
-                rect.setFill(nextAlive ? Color.LIGHTGREEN : Color.BLACK);
-            });
-        }
-    }
-
-    protected int countAliveNeighbors(int x, int y) {
-        int aliveCount = 0;
-
-        for (int yOffset = -1; yOffset <= 1; yOffset++) {
-            for (int xOffset = -1; xOffset <= 1; xOffset++) {
-                if (xOffset == 0 && yOffset == 0) {
-                    continue;
-                }
-
-                int neighborX = (x + xOffset + GRID_WIDTH) % GRID_WIDTH;
-                int neighborY = (y + yOffset + GRID_HEIGHT) % GRID_HEIGHT;
-
-                int neighborIndex = neighborY * GRID_WIDTH + neighborX;
-                Entity neighbor = cells.get(neighborIndex);
-
-                if (neighbor.getBoolean("alive")) {
-                    aliveCount++;
-                }
-            }
-        }
-
-        return aliveCount;
-    }
-
+    /**
+     * The main entry point for the application.
+     *
+     * @param args command-line arguments
+     */
     public static void main(String[] args) {
         launch(args);
     }
---
+++ src/test/java/se/peaccounting/GameLogicTest.java
@@ -0,0 +1,99 @@
+package se.peaccounting;
+
+import com.almasb.fxgl.entity.Entity;
+import javafx.scene.shape.Rectangle;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Test class for the GameLogic.
+ */
+public class GameLogicTest {
+
+    /**
+     * Creates a mock entity with the specified position and alive state.
+     *
+     * @param x     the x-coordinate of the entity
+     * @param y     the y-coordinate of the entity
+     * @param alive the alive state of the entity
+     * @return the created mock entity
+     */
+    private Entity createMockEntity(double x, double y, boolean alive) {
+        Entity entity = new Entity();
+        entity.setPosition(x, y);
+        entity.setProperty("alive", alive);
+        entity.getViewComponent().addChild(new Rectangle());
+        return entity;
+    }
+
+    /**
+     * Creates a list of mock entities with the specified grid dimensions and initial alive state.
+     *
+     * @param gridWidth     the width of the grid
+     * @param gridHeight    the height of the grid
+     * @param initialState  the initial alive state for all entities
+     * @return the list of created mock entities
+     */
+    private List<Entity> createMockEntities(int gridWidth, int gridHeight, boolean initialState) {
+        List<Entity> cells = new ArrayList<>();
+        for (int y = 0; y < gridHeight; y++) {
+            for (int x = 0; x < gridWidth; x++) {
+                cells.add(createMockEntity(x * 10, y * 10, initialState));
+            }
+        }
+        return cells;
+    }
+
+    @Test
+    public void countAliveNeighborsTest() {
+        int gridWidth = 3;
+        int gridHeight = 3;
+        List<Entity> cells = createMockEntities(gridWidth, gridHeight, false);
+
+        // Set the center cell to be alive
+        cells.get(4).setProperty("alive", true);
+
+        int aliveNeighbors = GameLogic.countAliveNeighbors(0, 0, cells, gridWidth, gridHeight);
+
+        assertEquals(1, aliveNeighbors);
+    }
+
+    @Test
+    public void updateCellsTest() {
+        int gridWidth = 3;
+        int gridHeight = 3;
+        List<Entity> cells = createMockEntities(gridWidth, gridHeight, false);
+        List<Boolean> nextGeneration = new ArrayList<>();
+
+        // Set the next generation to be all alive
+        for (int i = 0; i < cells.size(); i++) {
+            nextGeneration.add(true);
+        }
+
+        GameLogic.updateCells(cells, nextGeneration, 10);
+
+        // Check if all cells are updated to be alive
+        for (Entity cell : cells) {
+            assertTrue(cell.getBoolean("alive"));
+        }
+    }
+
+    @Test
+    public void computeNextGenerationTest() {
+        int gridWidth = 3;
+        int gridHeight = 3;
+        List<Entity> cells = createMockEntities(gridWidth, gridHeight, false);
+
+        // Set the center cell to be alive
+        cells.get(4).setProperty("alive", true);
+
+        List<Boolean> nextGeneration = GameLogic.computeNextGeneration(cells, 10, gridWidth, gridHeight);
+
+        assertEquals(cells.size(), nextGeneration.size());
+    }
+}
